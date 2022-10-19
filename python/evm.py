668901c2e3ec9b65986f67643748d8a633507bf9
karmacoma <karma@coma.lol>
2022-10-19 16:56:03 -0700

    Update Python template
    - check that the stack content matches (deep equality check)
    - parse hex values in `test['expect']['stack']`
    - locate evm.json even if we run from a different folder
    - cosmetics: format strings, emojis in output

--- python/evm.py
+++ python/evm.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 import json
+import os
 
 def evm(code):
     pass
 
 def test():
-    with open('../evm.json') as f:
+    script_dirname = os.path.dirname(os.path.abspath(__file__))
+    json_file = os.path.join(script_dirname, "..", "evm.json")
+    with open(json_file) as f:
         data = json.load(f)
         total = len(data)
 
         for i, test in enumerate(data):
-            print("Test #" + str(i + 1), "of", total, test['name'])
-
             # Note: as the test cases get more complex, you'll need to modify this
             # to pass down more arguments to the evm function
             code = bytes.fromhex(test['code']['bin'])
             stack = evm(code)
 
-            expected_stack = [int(x) for x in test['expect']['stack']]
-
-            if stack != expected_stack:
+            expected_stack = [int(x, 16) for x in test['expect']['stack']]
+            
+            # if any value differs:
+            if any(x != y for x, y in zip(stack, expected_stack)):
+                print(f"❌ Test #{i + 1}/{total} {test['name']}")
                 print("Stack doesn't match")
                 print(" expected:", expected_stack)
                 print("   actual:", stack)
@@ -27,10 +30,11 @@ def test():
                 print("Test code:")
                 print(test['code']['asm'])
                 print("")
-                print("Progress: " + str(i) + "/" + str(len(data)))
+                print(f"Progress: {i}/{len(data)}")
                 print("")
                 break
-
+            else:
+                print(f"✅ Test #{i + 1}/{total} {test['name']}")
 
 if __name__ == '__main__':
     test()

613523f1bc06202293fc8394999410b81cbbe853
w1nt3r-eth <w1nt3r.eth@gmail.com>
2022-11-06 20:11:19 -0800

    More opcodes

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,15 @@ const path = require('path');
 const yaml = require('yaml');
 
 function main() {
+  const seenOpcodes = new Set();
+  const lookAtOpcodes = (code) => code.forEach((line) => seenOpcodes.add(line.match(/^(\w+).*/)[1].toUpperCase()));
+
   const content = fs.readFileSync(path.join(__dirname, 'evm.yaml'), 'utf8');
   const tests = yaml.parse(content, (key, value) => {
     // Compile assembly code to bytecode, keeping the original source code
     if (key === 'code') {
       if (Array.isArray(value)) {
+        lookAtOpcodes(value);
         return { asm: value.join('\n'), bin: uint8ArrayToHexString(assembler(value.join('\n'))) };
       } else {
         return { asm: null, bin: value };
@@ -35,11 +39,17 @@ function main() {
 
   const ordered = Object.entries(tests).map(([name, t]) => ({ name, ...t }));
 
+  Object.keys(assembler.opcodes).forEach((name) => {
+    if (!seenOpcodes.has(name) && ['PUSH', 'SWAP', 'DUP'].every((prefix) => !name.startsWith(prefix))) {
+      console.log(`Warning: no tests for opcode ${name}`);
+    }
+  });
+
   fs.writeFileSync(path.join(__dirname, '..', 'evm.json'), JSON.stringify(ordered, null, 2));
 }
 
 function parseYamlBigInt(value) {
-  if (typeof value === 'string' && value.match(/^(0x)?[0-9a-f_]+n$/)) {
+  if (typeof value === 'string' && value.match(/^(0x)?[0-9a-f_]+n$/i)) {
     return '0x' + BigInt(value.replace(/n$/, '').replace(/_/g, '')).toString(16);
   }
   return value;
